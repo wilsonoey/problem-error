f547c484e6e4343b37b2b34876e2a990e9d07ded
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2022-08-13 15:39:08 +0800

    Delete Dicoding/Belajar Membuat Aplikasi Web dengan React/1 directory

--- Dicoding/Belajar Membuat Aplikasi Web dengan React/1/src/components/Search/ActionButtonSearch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ActionButtonSearch({ onSearch }) {
-  return (
-    <input class="note-search" type="text" placeholder="Search Name" onChange={(event) => onSearch(event.target.value)} />
-  );
-}
-
-export default ActionButtonSearch;
