eb6b6a0853dba1deed6e401bcdf0e137d6e15c80
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-01-19 18:39:16 +0800

    Update login.js

--- Dicoding/Belajar_Fundamental_Aplikasi_Web_dengan_React/Problem5FR/src/views/login.js
+++ Dicoding/Belajar_Fundamental_Aplikasi_Web_dengan_React/Problem5FR/src/views/login.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { login } from '../utils/api'
 import { useNavigate } from 'react-router-dom'
 import FormLogin from '../components/forms/form-login'
 
-function Login({ loginSuccess, auth }) {
+function Login({ loginSuccess }) {
   const navigate = useNavigate();
   async function onLoginHandler(user) {
     const { error, data } = await login(user);
     if (!error) {
       loginSuccess(data);
-      navigate('/');
-    } else if (auth !== (undefined || null)) {
-      alert("User already logged in!");
-      navigate('/homewith-auth');
+      navigate('/dashboard');
     }
   }
+  
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      navigate('/dashboard');
+    }
+  });
 
   return (<><FormLogin login={onLoginHandler} /></>);
 }
