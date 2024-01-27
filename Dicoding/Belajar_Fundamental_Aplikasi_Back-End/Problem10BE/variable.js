9b71c168353d9fcb5bc4b8616eb4e297fae1146e
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-01-27 13:59:04 +0700

    Add files via upload

---
+++ Dicoding/Belajar_Fundamental_Aplikasi_Back-End/Problem10BE/variable.js
@@ -0,0 +1,12 @@
+const variable = {
+  HOSTDB: 'localhost',
+  USERDB: 'root',
+  PASSDB: '',
+  NAMEDB: 'wilsonoey_kamiada',
+  ACCESSJWT: 'GET_FROM_node->require('crypto').randomBytes(64).toString('hex');_IN_TERMINAL',
+  REFRESHJWT: 'GET_FROM_node->require('crypto').randomBytes(64).toString('hex');_IN_TERMINAL',
+  ACCESS_TOKEN_AGE: 86400,
+  NODE_ENV: 'development',
+}
+
+module.exports = variable;
