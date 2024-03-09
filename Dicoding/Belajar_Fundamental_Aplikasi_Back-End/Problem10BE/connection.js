0de965856283961d897b8ee4fd444f9aaff64a46
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-03-09 19:13:13 +0800

    Update connection.js

--- Dicoding/Belajar_Fundamental_Aplikasi_Back-End/Problem10BE/connection.js
+++ Dicoding/Belajar_Fundamental_Aplikasi_Back-End/Problem10BE/connection.js
@@ -8,7 +8,7 @@ const connection = createConnection({
   database: variable.NAMEDB,
 });
 connection.connect((checker) => console.log(checker));
-// tampilkan di terminal setiap tindakan database menggunakan method yang ada
+
 connection.on('enqueue', (sequence) => (sequence.constructor.name === 'Query' ? console.log(sequence.sql) : null));
 
 module.exports = connection;
